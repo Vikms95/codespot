8c5cf06955ead93c9f57d8e8a8f49d286e9a3d32
Vikms95 <vms1995@hotmail.com>
2022-08-25 05:17:39 +0200

    refactor getPostToUpdate with new services

--- client/src/services/getPostToUpdate.js
+++ client/src/services/getPostToUpdate.js
@@ -1,16 +1,12 @@
+import { getFromStorage } from './getFromStorage'
 import { getPost } from './getPost'
 import { setToStorage } from './setToStorage'
 export const getPostToUpdate = (posts, postid) => {
-  let postToUpdate
-
   if (posts.length) {
-    postToUpdate = getPost(posts, postid)
+    const postToUpdate = getPost(posts, postid)
     setToStorage('postToUpdate', postToUpdate)
 
     return postToUpdate
-  } else {
-    postToUpdate = JSON.parse(localStorage.getItem('posttoupdate'))
-
-    return postToUpdate
   }
+  return getFromStorage('postToUpdate')
 }
