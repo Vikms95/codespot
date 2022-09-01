1de589491cf55ee47b00c329a40f91967a6afd20
Vikms95 <vms1995@hotmail.com>
2022-09-01 02:47:52 +0200

    restyle comment form

--- client/src/components/Comment/CommentForm.jsx
+++ client/src/components/Comment/CommentForm.jsx
@@ -8,6 +8,7 @@ import { Button } from '../../style/Button';
 import { createComment } from '../../services/createComment';
 
 const StyledCommentForm = styled.form`
+	position: relative;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
@@ -16,13 +17,35 @@ const StyledCommentForm = styled.form`
 `;
 
 const StyledCommentInput = styled.textarea`
-	border-radius: 5px;
+	border-radius: 15px;
 	resize: none;
-	min-width: 60em;
+	min-width: 50em;
 	min-height: 10em;
 	padding: 2em;
+	padding-right: 15em;
 	outline: 1px solid #6649b8;
+
+	&::-webkit-scrollbar {
+		width: 0.75rem;
+	}
+
+	&::-webkit-scrollbar-track {
+		background: rgb(255, 255, 255);
+		border-radius: 10px;
+	}
+
+	&::-webkit-scrollbar-thumb {
+		background: #6649b8;
+		border-radius: 10px;
+	}
 `;
+
+const CommentFormButton = styled(Button)`
+	position: absolute;
+	bottom: 10%;
+	right: 3%;
+`;
+
 export function CommentForm(props) {
 	const { postid, setComments, parentid } = props;
 	const { user: userid } = useAuth();
@@ -46,7 +69,7 @@ export function CommentForm(props) {
 				value={text}
 				onChange={handleChange}
 			/>
-			<Button type='submit'> Publish comment </Button>
+			<CommentFormButton type='submit'> Comment </CommentFormButton>
 		</StyledCommentForm>
 	);
 }
--- client/src/services/createComment.js
+++ client/src/services/createComment.js
@@ -3,7 +3,9 @@ import { getCurrentDate } from "../utils/getCurrentDate";
 
 export const createComment = async (text, postid, userid, parentid) => {
   const timestamp = getCurrentDate();
-  const parent = parentid || ''
+  // We check if the comment has a parentid and attach it to the object,
+  // otherwise give the null value to say this is a root comment
+  const parent = parentid || null
 
   const response = await fetch('/api/comment',
     userCreateOptions('POST', { text, postid, userid, timestamp, parent })
